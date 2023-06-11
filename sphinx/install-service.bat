8c92371a078f521f16999b4cfae6ad449eb55a86
ferraro2 <ferrarotpp2@gmail.com>
2023-06-11 19:37:40 -0400

    Add sphinx conf and bat files

---
+++ sphinx/install-service.bat
@@ -0,0 +1,1 @@
+C:\Users\admin\Documents\main\installs\sphinx-2.2.11-release-win64\bin\searchd.exe --install --config C:\Users\admin\Documents\main\dev\tpp\amie\sphinx\sphinx.conf
---
+++ sphinx/manual_index.bat
@@ -0,0 +1,1 @@
+C:\Users\admin\Documents\main\installs\sphinx-2.2.11-release-win64\bin\indexer.exe --config C:\Users\admin\Documents\main\dev\tpp\amie\sphinx\sphinx.conf tppMain tppDelta1 tppDelta2 tppDelta3 tppDelta4 tppDelta5 tppDelta6 --rotate
