dd997efc606fc7c22ac89a8582c029c851206219
ferraro2 <ferrarotpp2@gmail.com>
2023-06-16 06:01:06 -0400

    Fix debug flag in php

--- www/logs/siteDefinitions.php
+++ www/logs/siteDefinitions.php
@@ -7,7 +7,7 @@
 
     include 'credentials.php';
 
-    if (LOCAL) {
+    if (DEBUG) {
         error_reporting(E_ALL);
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
